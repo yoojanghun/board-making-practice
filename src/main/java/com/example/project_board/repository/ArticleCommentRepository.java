22187652872dec2c19e2a6a7f77e8310bea1720e
yoojanghun <yoojanghun@naver.com>
2026-01-13 21:02:43 +0900

    #7 - data rest 설정

--- src/main/java/com/example/project_board/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/project_board/repository/ArticleCommentRepository.java
@@ -2,6 +2,9 @@ package com.example.project_board.repository;
 
 import com.example.project_board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+
 }
--- src/main/java/com/example/project_board/repository/ArticleRepository.java
+++ src/main/java/com/example/project_board/repository/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.example.project_board.repository;
 
 import com.example.project_board.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
