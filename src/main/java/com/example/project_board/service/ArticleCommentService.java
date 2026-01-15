e01a38194252c973800c71589f54e14938330b28
yoojanghun <yoojanghun@naver.com>
2026-01-15 15:26:54 +0900

    댓글 서비스 로직의 테스트와 골격 잡기

---
+++ src/main/java/com/example/project_board/service/ArticleCommentService.java
@@ -0,0 +1,27 @@
+package com.example.project_board.service;
+
+import com.example.project_board.dto.ArticleCommentDto;
+import com.example.project_board.repository.ArticleCommentRepository;
+import com.example.project_board.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+
+    }
+}
