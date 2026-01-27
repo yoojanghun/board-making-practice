d282852f0f614267e3d1219e4d431585e8c2120c
yoojanghun <yoojanghun@naver.com>
2026-01-28 01:56:17 +0900

    data 저장

--- src/main/java/com/example/demo/controller/ArticleCommentController.java
+++ src/main/java/com/example/demo/controller/ArticleCommentController.java
@@ -2,12 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.ArticleCommentDto;
 import com.example.demo.dto.response.ArticleCommentResponse;
-import com.example.demo.dto.security.BoardPrincipal;
 import com.example.demo.service.ArticleCommentService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
